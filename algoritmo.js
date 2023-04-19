2bc8ed81bd6851f7118b168d9f573d33f112685f
Antonio Gennaro <antgennaro@hotmail.com>
2023-04-19 12:53:36 -0300

    querySelector Ej IF#3(Bingo Mejorado)v2y3

--- algoritmo.js
+++ algoritmo.js
@@ -9,24 +9,53 @@ const max = 5; /* cuanto más chico, aumenta la probabilidad de ganar */
 
 // Selecciona el elemento con el id "miBoton"
 const palancaSlot = document.querySelector("#palancaSlot");
+const miBody      = document.querySelector("#mi-body");
+const BingoFijo   = document.querySelector("#mi-h1_1");
+const TuNumero    = document.querySelector("#mi-h1_2");
+const MsgResult   = document.querySelector("#mi-h1_3");
+const MsgPremio   = document.querySelector("#mi-h2_1");
+const MsgFinal    = document.querySelector("#mi-h2_2");  
+var IntervaloParp = 0;
 
-// ejecuta la función qu egenera el número aleatorio y chequea el resultado 
+// Al darle a la Palanca se ejecuta la función que genera el número aleatorio y chequea el resultado 
 palancaSlot.addEventListener("click", () => {
     let primerNumero = Math.floor(Math.random() * (max - min + 1)) + min;
     let segundoNumero = Math.floor(Math.random() * (max - min + 1)) + min;
     let tercerNumero = Math.floor(Math.random() * (max - min + 1)) + min;
-    document.write ("<h2>Tu número es:     | "+primerNumero+" | "+segundoNumero+" | "+tercerNumero+" | </h2"); 
-    document.write("<h2></h2>");
+    clearInterval(IntervaloParp);                 /* Frena cualquier parpadeo */
+	MsgResult.style.visibility = 'visible';       /* Pone Visible el texto */
+    BingoFijo.textContent    = "+ + + B I N G O - C F P 5 + + +" ;
+    TuNumero.textContent     = "Tu número es: | "+primerNumero+" | "+segundoNumero+" | "+tercerNumero+" | ";
+    
     if((primerNumero == segundoNumero) && (primerNumero == tercerNumero)){
-        document.write ("<h1>***     ¡¡¡BINGO!!!     ***</h1>");  
-        document.write ("<h1> Ganaste " +PREMIO_3_NROS_IGUALES+"</h1>");
+        miBody.style.backgroundColor = "red";
+        MsgResult.textContent = "***     PALANCAZOOOOO!!!     ***"; 
+        IntervaloParp = setInterval (function() {
+            MsgResult.style.visibility = (MsgResult.style.visibility == 'hidden' ? '' : 'hidden' );
+        }, 150); 
+        MsgPremio.textContent = "Ganaste " +PREMIO_3_NROS_IGUALES+"";
+        MsgFinal.textContent  = "Sigue así, hay más!!!!";
+        palancaSlot.textContent = "Vamos! Meté otro Palancazo!"; 
     }
     else if(((primerNumero == segundoNumero) ||(primerNumero == tercerNumero)) || (segundoNumero == tercerNumero)) {
-        document.write ("<h2>¡Te felicito! Ganaste  " +PREMIO_2_NROS_IGUALES+"</h2>");  
+        miBody.style.backgroundColor = "blue";
+        MsgResult.textContent = "¡Te felicito !";  
+        IntervaloParp = setInterval (function() {
+            MsgResult.style.visibility = (MsgResult.style.visibility == 'hidden' ? '' : 'hidden' );
+        }, 500);
+        MsgPremio.textContent = "Ganaste " +PREMIO_2_NROS_IGUALES+"";
+        MsgFinal.textContent  = "Vamos!! Puedes mejorar!";
+       palancaSlot.t = "Intentá de nuevo, dale!"; 
+       palancaSlot.textContent = "Intentá de Nuevo, dale!"; 
     }
     else {
-        document.write ("<h2>Lo sentimos, no has tenido suerte.</h3>"); 
-        document.write ("<h3>Pero igual ganaste un " +PREMIO_CONSUELO+" !</h3>"); 
-        document.write ("<h3>No abandones, sigue participando...</h3>"); 
+        miBody.style.backgroundColor = "grey";
+        MsgResult.textContent = "No has tenido suerte..."; 
+        IntervaloParp = setInterval (function() {
+            MsgResult.style.visibility = (MsgResult.style.visibility == 'hidden' ? '' : 'hidden' );
+        }, 2000); 
+        MsgPremio.textContent = "Pero igual ganaste un " +PREMIO_CONSUELO+"";
+        MsgFinal.textContent  = "No abandones, sigue participando..."; 
+        palancaSlot.textContent = "Dale despacito, vamos..."; 
     };  
 });
